4e5b6e4b497daf5d21631b0604f74fe05727415c
yair90segal <184441219+yair90segal@users.noreply.github.com>
2024-12-17 17:13:33 +0200

    added the function char_count to main.py

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ def main():
 
     print(word_count(file_contents))
 
+    print(char_count(file_contents))
+
 def get_text_by_path(path):
     with open(path) as f:
         return f.read()
 
 def word_count(text):
     return len(text.split())
+
+def char_count(text):
+    char_dict = {}
+    for ch in text:
+        ch_lower = ch.lower()
+        if ch_lower in char_dict:
+            char_dict[ch_lower] += 1
+        else:
+            char_dict[ch_lower] = 1
+    return char_dict
 
 main()
