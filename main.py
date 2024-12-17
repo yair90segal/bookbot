f05e4ab60ef1d4d309984c35566e103306af5a8f
yair90segal <184441219+yair90segal@users.noreply.github.com>
2024-12-17 16:55:38 +0200

    added the function get_text_by_path and word_count to main, and tested them

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+    path = "books/frankenstein.txt"
+    file_contents = get_text_by_path(path)
+    print(file_contents)
+
+    print(word_count(file_contents))
+
+def get_text_by_path(path):
+    with open(path) as f:
+        return f.read()
+
+def word_count(text):
+    return len(text.split())
+
+main()
